4cda2cd080f249aa028034e78b5a7bfa6a56424f
arnibjo <abo25@hi.is>
2024-03-11 15:21:52 +0000

    Program made from UML

---
+++ src/main/java/is/hi/hbv202g/assignment8/Book.java
@@ -0,0 +1,46 @@
+package is.hi.hbv202g.assignment8;
+
+import java.util.List;
+
+public class Book {
+    private String title;
+    private List<Author> authors;
+
+    public Book(String title, String authorName) {
+        this.title = title;
+        this.authors = List.of(new Author(authorName));
+    }
+
+    public Book(String title, List<Author> authors) throws EmptyAuthorListException {
+        this.title = title;
+        if (authors.isEmpty()) {
+            throw new EmptyAuthorListException("The author list cannot be empty.");
+        }
+        this.authors = authors;
+    }
+    
+
+    
+    public List<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<Author> authors) throws EmptyAuthorListException {
+        if (authors.isEmpty()) {
+            throw new EmptyAuthorListException("The author list cannot be empty.");
+        }
+        this.authors = authors;
+    }
+
+    public void addAuthor(Author author) {
+        authors.add(author);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
--- src/main/java/is/hi/hbv202g/assignment8/LibrarySystem.java
+++ src/main/java/is/hi/hbv202g/assignment8/LibrarySystem.java
@@ -1,3 +1,96 @@
 package is.hi.hbv202g.assignment8;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LibrarySystem {
+    private List<Book> books;
+    private List<Lending> lendings;
+    private List<User> users;
+
+    public LibrarySystem() {
+        books = new ArrayList<Book>();
+        lendings = new ArrayList<Lending>();
+        users = new ArrayList<User>();
+    }
+
+    public void addBookWithTitleAndNameOfSingleAuthor(String title, String authorName){
+        books.add(new Book(title, authorName));
+    }
+
+    public void addBookWithTitleAndAuthorList(String title, List<Author> authors) throws EmptyAuthorListException {
+        books.add(new Book(title, authors));
+    }
+    
+    public void addStudentUser(String name, boolean feePaid){
+        users.add(new Student(name, feePaid));
+    }
+
+    public void addFacultyMemberUser(String name, String department){
+        users.add(new FacultyMember(name, department));
+    }
+
+    public Book findBookByTitle(String title) throws UserOrBookDoesNotExistException {
+        for (Book book : books) {
+            if (book.getTitle().equals(title)) {
+                return book;
+            }
+        }
+        throw new UserOrBookDoesNotExistException("The book with the title " + title + " does not exist in the library system.");
+    }
+
+    public User findUserByName(String name) throws UserOrBookDoesNotExistException {
+        for (User user : users) {
+            if (user.getName().equals(name)) {
+                return user;
+            }
+        }
+        throw new UserOrBookDoesNotExistException("The user with the name " + name + " does not exist in the library system.");
+    }
+
+    public void borrowBook(User user, Book book) throws Exception {
+        if(users.indexOf(user) < 0){
+            throw new UserOrBookDoesNotExistException("The user with the name " + user.getName() + " does not exist in the library system.");
+        }
+        if(books.indexOf(book) < 0){
+            throw new UserOrBookDoesNotExistException("The book with the title " + book.getTitle() + " does not exist in the library system.");
+        }
+        Lending lending = new Lending(user, book);
+        for (Lending lend : lendings) {
+            if (lend.getBook().equals(book)) {
+                throw new Exception( "The book with the title " + book.getTitle() + " has already been borrowed.");
+            }
+        }
+        lendings.add(lending);
+        
+    }
+
+    public void extendLending(FacultyMember facultyMember, Book book, LocalDate newDueDate) throws UserOrBookDoesNotExistException {
+        boolean isBookBorrowed = false;
+        for (Lending lending : lendings) {
+            if (lending.getBook().equals(book)) {
+                lending.setDueDate(newDueDate);
+                isBookBorrowed = true;
+            }
+        }
+        if (!isBookBorrowed) {
+            if(books.indexOf(book) < 0) {
+                throw new UserOrBookDoesNotExistException("The book with the title " + book.getTitle() + " does not exist in the library system.");
+            }
+            else {
+                throw new UserOrBookDoesNotExistException("The book with the title " + book.getTitle() + " has not been borrowed yet.");
+            }
+        }
+    }
+
+    public void returnBook(User user, Book book) throws UserOrBookDoesNotExistException {
+        if(users.indexOf(user) < 0){
+            throw new UserOrBookDoesNotExistException("The user with the name " + user.getName() + " does not exist in the library system.");
+        }
+        if(books.indexOf(book) < 0){
+            throw new UserOrBookDoesNotExistException("The book with the title " + book.getTitle() + " does not exist in the library system.");
+        }
+        lendings.remove(new Lending(user, book));
+    }
 }
